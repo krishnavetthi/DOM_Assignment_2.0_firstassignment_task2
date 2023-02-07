b5dd9dfbdf173ce62e52f29fed7e6ab6128ec2f1
krishnavetthi <krishnavetthib@outlook.com>
2023-02-06 23:07:27 -0500

    first commit

---
+++ task2Output.js
@@ -0,0 +1,7 @@
+var header = document.querySelector("header");
+var element = header.getElementsByTagName("li")[2];
+element.querySelector('a').textContent="Projects";
+
+var footer = document.querySelector("footer");
+var list = footer.querySelector("ul");
+list.style.display="none";
